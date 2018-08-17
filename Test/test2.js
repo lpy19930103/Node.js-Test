72db5d706d4adace4b8658e8eb6a9f1cfce8be24
lipengyuan <lipengyuan>
2018-08-17 17:26:11 +0800

    爬虫

--- Test/test2.js
+++ Test/test2.js
@@ -2,9 +2,15 @@ const superagent = require('superagent');
 const cheerio = require('cheerio');
 const Eventproxy = require('eventproxy');
 const url = require('url');
+const async = require('async');
+var homeInfo = require('./test3');
+const cnodeUrl = 'https://hz.lianjia.com/ershoufang/';
 
-const cnodeUrl = 'http://www.howzf.com';
-
+var fetchUrl = (url, callback) => {
+    homeInfo(url, (err, info) => {
+        callback(err, info);
+    });
+};
 
 superagent.get(cnodeUrl + '/esfn/EsfnSearch_csnew.jspx')
     .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8')
@@ -14,44 +20,45 @@ superagent.get(cnodeUrl + '/esfn/EsfnSearch_csnew.jspx')
         if (err) return next(err);
         let $ = cheerio.load(sres.text);
         let items = [];
-        $("h5 a[target='_blank']").each((idx, element) => {
+        $("div.info a[target='_blank']").each((idx, element) => {
             let $element = $(element);
             var resolve = url.resolve(cnodeUrl, $element.attr('href'));
-            if ($element.attr('href').startsWith("/esf")) {
-                items.push({
-                    href: resolve,
-                    title: $element.attr('title')
-                });
-            }
-        });
-        var eventproxy = new Eventproxy();
-        eventproxy.after('topic_html', items.length, (topics) => {
-            topics.map((topicPair) => {
-                var info = topicPair[0];
-                let topicHtml = topicPair[1];
-                let $ = cheerio.load(topicHtml);
-                $("#myUl .hover").click(() => {
-                    $('#mapsearchlist.fr').text();
-                });
-
-                return ({
-                    href: info['href'],
-                    title: info['title'],
-                });
-            });
-            console.log('final:');
-            console.log(topics);
+            items.push(resolve);
         });
 
-        items.forEach((topicUrl) => {
-            superagent.get(topicUrl['href'])
-                .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8')
-                .set('Referer', 'http://www.howzf.com/')
-                .set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36')
-                .end((err, res) => {
-                    console.log('fetch ' + topicUrl['href'] + ' successful');
-                    eventproxy.emit('topic_html', [topicUrl, res.text])
-                });
+        async.mapLimit(items, 5, (url, callback) => {
+            fetchUrl(url, callback)
+        }, (err, result) => {
+            console.log(err);
+            console.log(result);
         });
+        /* var eventproxy = new Eventproxy();
+         eventproxy.after('topic_html', items.length, (topics) => {
+             topics.map((topicPair) => {
+                 var info = topicPair[0];
+                 let topicHtml = topicPair[1];
+                 let $ = cheerio.load(topicHtml);
+                 $("#myUl .hover").click(() => {
+                     $('#mapsearchlist.fr').text();
+                 });
+
+                 return ({
+                     href: info['href'],
+                     title: info['title'],
+                 });
+             });
+             console.log('final:');
+             console.log(topics);
+         });
+
+         items.forEach((topicUrl) => {
+             superagent.get(topicUrl['href'])
+                 .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*!/!*;q=0.8')
+                 .set('Referer', 'http://www.howzf.com/')
+                 .set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36')
+                 .end((err, res) => {
+                     console.log('fetch ' + topicUrl['href'] + ' successful');
+                     eventproxy.emit('topic_html', [topicUrl, res.text])
+                 });
+         });*/
     });
-
--- Test/test3.js
+++ Test/test3.js
@@ -1,15 +1,43 @@
 const superagent = require('superagent');
 const cheerio = require('cheerio');
-var url = 'http://www.howzf.com/esf/xq_csnew_80619972.htm';
-superagent.get(url)
-    .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8')
-    .set('Referer', 'http://www.howzf.com/')
-    .set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36')
-    .end((err, res) => {
-        if (err) {
-            return console.log(err)
-        }
-        let $ = cheerio.load(res.text);
-        console.log($("div.infopic_box").attr());
+// var url = 'https://hz.lianjia.com/ershoufang/103102699616.html';
 
-    });
+
+var homeInfo = (url, callback) => {
+    superagent.get(url)
+        .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8')
+        .set('Referer', 'http://www.howzf.com/')
+        .set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36')
+        .end((err, res) => {
+            if (err) {
+                return console.log(err)
+            }
+            let $ = cheerio.load(res.text);
+            let homeInfos = [];
+            let listingInfos = [];
+            $("div.introContent div.base ul li").each((idx, element) => {
+                let $element = $(element);
+                homeInfos.push($element.text());
+            });
+            $("div.introContent div.transaction ul li").each((idx, element) => {
+                let $element = $(element);
+                listingInfos.push($element.find("span").text().replace(/\ +/g, "").replace(/[\r\n]/g, ""));
+            });
+            var info = ({
+                title: $("div.title h1.main").text(),
+                subTitle: $("div.title div.sub").text(),
+                price: `总价${$("div.price span.total").text()} ${$("div.price span.unit span").text()} 单价${$("div.price span.unitPriceValue").text()}`,
+                firstPay: $("div.price span.taxtext").text(),
+                homeTag: $("div.newwrap div.tags div.content").text().replace(/\ +/g, "").replace(/[\r\n]/g, ","),
+                otherInfo: $("div.newwrap div.introContent div.baseattribute div.content").text().replace(/\ +/g, "").replace(/[\r\n]/g, ","),
+                homeInfos: homeInfos,
+                listingInfos: listingInfos,
+
+            });
+            callback('success', info);
+            console.log(info);
+            return info;
+        });
+};
+
+module.exports = homeInfo;
